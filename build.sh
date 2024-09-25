c2dbe363ae8afe36b75faecb01edc9a83dcdca13
krishnakumari200424 <goddallakrishnakumari@gmail.com>
2024-09-25 14:57:32 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Building the web application....."
+# Add any build steps here, for example:
+# npm install or mvn package
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Running tests...."
+# Add any test commands here, for example:
+# npm test or mvn test
